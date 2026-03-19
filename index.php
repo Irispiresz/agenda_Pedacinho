11549d9cd0a86988b82b029d9426805a19571328
Irispiresz <162588398+Irispiresz@users.noreply.github.com>
2026-03-19 08:53:40 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/custom.css" rel="stylesheet">
+    <title>Document</title>
+    
+
+     <script>
+   
+  
+
+    </script>
+
+
+
+</head>
+<body>
+
+    <div id='calendar'></div>
+    <script src='dist/index.global.min.js'></script>
+    <script src='js/core/locales-all.global.min.js'></script>
+    <script src="js/custom.js"></script>
+
+</body>
+</html>
