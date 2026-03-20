0dc49a9668fc8e7a38cd86571444364c0e010337
Irispiresz <162588398+Irispiresz@users.noreply.github.com>
2026-03-20 08:32:38 -0300

    Add files via upload

---
+++ pastaagenda/indexagenda.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="../css_pda/style_pda.css">
+    <link rel="stylesheet" href="../css_pda/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css_pda/fontawesome/css/all.min.css">
+   <title>Agenda Pedacinho</title>
+</head>
+<body>
+    <header>
+     <h1>Lista de Agendas</h1>
+    </header>
+   
+    <!-- Relogio de cima-->
+    <div class="relogio-container">
+         <div id="relogio">
+    </div>
+
+   
+
+        <!-- Modal para add/edit,deletar-->
+         <div id="seletorEventoWrapper">
+            <label for="seletorEvento">
+                <strong>Selecione</strong>
+
+
+    <div id='calendar'></div>
+    <script src="dist/index.global.js"></script>
+    <script src="js/core/locales-all.global.min.js"></script>
+    <script src="js/custom.js"></script>
+
+</body>
+</html>
